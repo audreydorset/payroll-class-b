07c2f7e698b38a2865bfe09d5b62643248f7c54d
John Rowley <outsidethebox@eircom.net>
2020-09-23 16:17:11 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PayrollClassA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
